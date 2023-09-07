0ee1eca8254ca299e2336e0b4ec2739f5de74ed9
Tahmidur_Rabb <tr1476@nyu.edu>
2023-09-06 20:44:19 -0400

    removing something

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import openai
-openai.api_key = "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"
+import gradio 
 
+
+messages = [{"role": "system", "content": "You are a psychologist"}]
+
+def customChatGPT(user_input):
+    messages.append({"role": "user", "content": user_input})
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = messages
+    )
+    chatGPT_reply = response["choices"][0]["message"]["content"]
+    messages.append({"role": "assistant", "content": ChatGPT_reply})
 
 completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Write an essay about penguins" }])
 print(completion.choices[0].message.content)
