f22a8a3b55d2bdce07b53e9800e85146923c6a3b
Tahmidur_Rabb <tr1476@nyu.edu>
2023-09-14 01:57:09 -0400

    Update

--- main.py
+++ main.py
@@ -2,19 +2,24 @@ import openai
 import gradio 
 
 
-messages = [{"role": "system", "content": "You are a psychologist"}]
 
-def customChatGPT(user_input):
+
+
+messages = [{"role": "system", "content": "You are a software engineer"}]
+
+def CustomChatGPT(user_input):
     messages.append({"role": "user", "content": user_input})
     response = openai.ChatCompletion.create(
         model = "gpt-3.5-turbo",
         messages = messages
     )
-    chatGPT_reply = response["choices"][0]["message"]["content"]
+    ChatGPT_reply = response["choices"][0]["message"]["content"]
     messages.append({"role": "assistant", "content": ChatGPT_reply})
+    return ChatGPT_reply
 
-completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Write an essay about penguins" }])
-print(completion.choices[0].message.content)
+demo = gradio.Interface(fn=CustomChatGPT, inputs = "text", outputs= "text", title="Career Coach")
+
+demo.launch(share=True)
 
 
 '''
