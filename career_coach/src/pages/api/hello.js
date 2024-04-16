efcdfd950b54fd9c3873870f8797d4dd37c43ff6
Tahmidur_Rabb <tr1476@nyu.edu>
2024-04-15 22:11:52 -0400

    Chatbot

--- career_coach/src/pages/api/hello.js
+++ career_coach/src/pages/api/hello.js
@@ -1,1 +1,24 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// import axios from 'axios';
+
+// export default async function handler(req,res) {
+//     if (req.method !== 'POST') {
+//         res.status(405).json({message: 'Message should be POST'});
+//     }
+//     else {
+//         try {
+//             const {body} = req;
+//             const url = 'https://api.openai.com/v1/chat/completions'; //Retrieving OpenAI completions 
+//             const headers = {                                    
+//                     'Content-type': 'application/json',
+//                     'Authorization': `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}` //Initiating Open-API key from env file 
+//                 };
+//             const response = await axios.post(url,data, {headers:headers})
+//             res.status(200).json(response);
+//         } catch (error) {
+//             console.log(error);
+//             res.status(500).json({message: 'Something went wrong'})
+//         }
+//     }
+
+// }
